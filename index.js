fc1a0077041e900c7351e105ad98f7b5fd194e9e
me <trycatching337@gmail.com>
2020-12-30 14:24:57 +0900

    저장소에 처음올림

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 7000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://201230:zxchjk12@mdb.7abqt.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+//  밑에것들 추가로 선언해줘야 오류 안생김
+useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
+}).then(()=> console.log('connect'))
+.catch(err => console.log(err))
+// 연결이 잘 되었다면(then) 실행
+// 연길이 안되었다면(catch) 실행
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
